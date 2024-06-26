f2e2fe295a08c6817f8a11e0131eaf0b3d545fa8
Mirasalm <mira.salmensaari@gmail.com>
2024-06-26 10:51:04 +0300

    Update hello.cpp for bonus exercise

--- mpi/hello-world/hello.cpp
+++ mpi/hello-world/hello.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+# include <mpi.h>
 
 int main(int argc, char *argv[])
 {
-    // TODO: say hello! in parallel
-    std::cout << "Hello!" << std::endl;
+    int size, rank;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    char name[MPI_MAX_PROCESSOR_NAME];
+    int namelen;
+
+    MPI_Get_processor_name(name, &namelen);
+
+    std::cout << "Hello from rank " << rank << " running on " << name  << "!";
+    
+    if (rank == 0) {
+        std::cout << " Number of MPI processes is " << size;
+    }
+
+    std::cout << std::endl;
+    MPI_Finalize();
 }
