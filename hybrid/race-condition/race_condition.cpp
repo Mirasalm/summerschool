a095341a2cc4d5f091b91d2e6de40d34af68608f
Mirasalm <mira.salmensaari@gmail.com>
2024-06-28 15:59:32 +0300

    race-condition: add solution

---
+++ hybrid/race-condition/race_condition.cpp
@@ -0,0 +1,37 @@
+#include <cstdio>
+
+#define NX 102400
+
+int main(void)
+{
+    long vecA[NX];
+    long sum, psum, sumex;
+    int i;
+    
+    
+    sum = 0.0;
+#pragma omp parallel
+{
+
+    /* Initialization of the vectors */
+    #pragma omp for
+    for (i = 0; i < NX; i++) {
+        vecA[i] = (long) i + 1;
+    }
+
+    sum = 0.0;
+
+    /* TODO: Parallelize computation */
+    #pragma omp for reduction(+:sum)
+    for (i = 0; i < NX; i++) {
+        sum += vecA[i];
+    }
+}
+    printf("Sum: %ld\n", sum);
+
+    sumex = (long) NX * (NX + 1) / ((long) 2);
+    printf("Arithmetic sum formula (exact): %ld\n", sumex);
+    printf("Difference: %li\n", sumex - sum);
+
+    return 0;
+}
