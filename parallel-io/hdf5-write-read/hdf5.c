ed4950d58c93cbb7c15214df49fe8aefc56cd69e
Mirasalm <mira.salmensaari@gmail.com>
2024-06-28 11:59:07 +0300

    hdf5: WIP, h5_writer maybe finished

--- parallel-io/hdf5-write-read/hdf5.c
+++ parallel-io/hdf5-write-read/hdf5.c
@@ -59,15 +59,46 @@ void h5_writer(int my_id, int *localvector, int localsize)
     /* Create the handle for parallel file access property list
        and create a new file */
 
+    // Create a new property list for file access
+    hid_t plist = H5Pcreate(H5P_FILE_ACCESS);
+    // Store MPI IO communicator info to the file access property list
+    H5Pset_fapl_mpio(plist, MPI_COMM_WORLD, MPI_INFO_NULL);
+
+    // Create a new HDF5 file named "vector_<my_id>.h5"
+    char* filename = malloc(sizeof(char)*10 + 10); // TODO: add better malloc logic, this should be good enough though.
+    printf(filename, "vector_%i.h5",my_id);
+    hid_t file = H5Fcreate(filename, H5F_ACC_TRUNC, H5P_DEFAULT, plist);
+
     /* Create the dataset */
 
+    // Create a new simple dataspace for the file and open for access
+    hid_t dataspace = H5Screate_simple(1, (const hsize_t[]){(hsize_t)localsize}, NULL);
+    // Create a new dataset named "Vectors" for 'file'
+    hid_t dataset = H5Dcreate(file, "Vectors", H5T_NATIVE_INT, dataspace, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+
     /* Select a hyperslab of the file dataspace */
+
+    // Number of blocks to be included in the hyperslab region
+    hsize_t count[] = {1};
+    // Select a hyperslab region of the file dataspace
+    H5Sselect_hyperslab(dataspace, H5S_SELECT_SET, (const hsize_t[]){(hsize_t)localsize}, NULL, count, NULL);
 
     /* Now we can write our local data to the correct position in the
        dataset. Here we use collective write, but independent writes are
        also possible. */
 
+    // Create a new simple dataspace for the memory buffer and open for access
+    hid_t memspace = H5Screate_simple(1, count, NULL);
+    plist_id = H5Pcreate(H5P_DATASET_XFER);
+
+    // Set data access to collective
+    H5Pset_dxpl_mpio(plist_id, H5FD_MPIO_COLLECTIVE);
+
+    H5Dwrite(dataset, H5T_NATIVE_INT, memspace, dataspace, plist_id, &localvector);
+
     /* Close all opened HDF5 handles */
+    H5Pclose(plist_id);
+
 }
 
 
