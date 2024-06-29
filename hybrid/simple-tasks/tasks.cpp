bb1b1959b6f704012c11178642e606291557b706
Mirasalm <mira.salmensaari@gmail.com>
2024-06-29 22:10:11 +0300

    simple tasks done

--- hybrid/simple-tasks/tasks.cpp
+++ hybrid/simple-tasks/tasks.cpp
@@ -14,12 +14,21 @@ int main(void)
 
     // TODO: launch threads and create tasks so that there 
     // one task per loop iteration 
-    for (int i=0; i < 4; i++) {
-           tid = omp_get_thread_num();
-           printf("Task %d executed by thread %d\n", i, tid);
-           array[i] += tid;
-    }
+    #pragma omp parallel default(none) private(tid) shared(array)
+    {
+                        
 
+        #pragma omp single
+        for (int i=0; i < 4; i++) {
+            #pragma omp task
+            {
+                tid = omp_get_thread_num();
+                printf("Task %d executed by thread %d\n", i, tid);
+                array[i] += tid;
+            }
+        }
+
+    }
     // TODO end
 
     printf("Array at the end: ");
